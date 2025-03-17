80f38425a0a8c894f5cb047f532a15bdd2cfdf91
Dharmesh Chawla <dchawla@enphaseenergy.com>
2025-03-17 18:32:33 +0530

    moved the code to start process function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-const connectDB = require("./db");
 const { isMarketOpen, startProcess } = require("./process");
 const scheduleJobs = require("./scheduler");
-const fetchLoginDetails = require("./controller/fetchLoginDetails");
 
 const main = async () => {
     try {
-        await connectDB();
-        await fetchLoginDetails();
-
         if (isMarketOpen()) {
             startProcess();
         } else {
--- src/process.js
+++ src/process.js
@@ -3,6 +3,8 @@ const { startWebSocket, stopWebSocket } = require("./controller/webSocket");
 const { API_KEY, INTERVAL_MS, TIMEZONE, MARKET_START_HOURS, MARKET_START_MINUTES, MARKET_END_HOURS, MARKET_END_MINUTES } = require("./config");
 const momentTz = require("moment-timezone");
 const OrderPlacer = require('./controller/placeOrder');
+const connectDB = require("./db");
+const fetchLoginDetails = require("./controller/fetchLoginDetails");
 
 
 let interval = null;
@@ -12,8 +14,11 @@ const isMarketOpen = () => {
     return now.isBetween(now.clone().hour(MARKET_START_HOURS).minute(MARKET_START_MINUTES), now.clone().hour(MARKET_END_HOURS).minute(MARKET_END_MINUTES));
 };
 
-const startProcess = () => {
+const startProcess = async () => {
     if (interval) return;
+
+    await connectDB();
+    await fetchLoginDetails();
 
     fetchCandleData();
     interval = setInterval(fetchCandleData, INTERVAL_MS);
